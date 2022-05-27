2f27fe7c34f8c369dc725e0ed47c53fcaa67c733
Your Name <you@example.com>
2022-05-27 18:29:06 +0600

    almost done the project

--- src/Pages/Dashboard/ManageProducts.js
+++ src/Pages/Dashboard/ManageProducts.js
@@ -7,53 +7,51 @@ import Spinner from '../../Shared/Spinner';
 import Product from './Product';
 
 const ManageProducts = () => {
-    const [confirmDelete, setConfirmDelete] = useState(null);
-    const [products, isLoading] = useProduct();
+  const [confirmDelete, setConfirmDelete] = useState(null);
+  const [products, isLoading] = useProduct();
 
-    if(isLoading){
-        return <Spinner></Spinner>
-    }
+  if (isLoading) {
+    return <Spinner></Spinner>;
+  }
 
-    const handleProductDelete = async (id) => {
-        const url = `http://localhost:5000/equipment/${id}`;
-        await axios.delete(url).then((res) => {
-          const { data } = res;
-          console.log(data);
-          if (data.result.deletedCount) {
-            toast.success(data.success);
-            setConfirmDelete(null);
-          }
-        });
-      };
-    
+  const handleProductDelete = async (id) => {
+    const url = `https://sleepy-anchorage-47167.herokuapp.com/equipment/${id}`;
+    await axios.delete(url).then((res) => {
+      const { data } = res;
+      console.log(data);
+      if (data.result.deletedCount) {
+        toast.success(data.success);
+        setConfirmDelete(null);
+      }
+    });
+  };
 
-    return (
-       <>
-        <div className="container-fluid py-12">
+  return (
+    <>
+      <div className="container-fluid py-12">
         <h1 className="text-center md:text-6xl font-thin text-primary text-3xl font-serif">
           Equipments
         </h1>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 justify-items-center gap-y-8">
-          {
-          products?.map((product) => <Product 
-          key={product?._id} 
-          product={product}
-          handleProductDelete={handleProductDelete}
-          setConfirmDelete={setConfirmDelete}
-          ></Product>
-          )
-          }
+          {products?.map((product) => (
+            <Product
+              key={product?._id}
+              product={product}
+              handleProductDelete={handleProductDelete}
+              setConfirmDelete={setConfirmDelete}
+            ></Product>
+          ))}
         </div>
       </div>
 
       {confirmDelete && (
-          <DeleteDialog
-            confirmDelete={confirmDelete}
-            handleProductDelete={handleProductDelete}
-          />
-        )}
-       </>
-    );
+        <DeleteDialog
+          confirmDelete={confirmDelete}
+          handleProductDelete={handleProductDelete}
+        />
+      )}
+    </>
+  );
 };
 
 export default ManageProducts;
