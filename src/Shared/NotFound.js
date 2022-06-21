03b0cdb12620532fb650d6739898fe661926b970
Your Name <you@example.com>
2022-06-21 14:34:13 +0600

    check

--- src/Shared/NotFound.js
+++ src/Shared/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
     return (
         <div className='h-screen flex justify-center items-center'>
 
-                <div>
+                <div className=''>
                     <h1 className=' text-9xl font-bold text-center text-primary'>404</h1>
                     <p className='text-7xl text-error'>Page Not Found</p>
                     <button className='btn btn-outline btn-accent px-12  mt-6'>
